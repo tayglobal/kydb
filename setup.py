4f54712ea73996e3dc0f1b82a410e162c384ceee
Tony Yum <tony.yum@gmail.com>
2020-09-01 20:37:26 +0000

    fixed autopep8

--- setup.py
+++ setup.py
@@ -4,27 +4,27 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-  name = 'kydb',
-  packages = ['kydb'],
-  version = '0.2.2',
-  license='MIT',
-  description = 'kydb (Kinyu Database). NoSQL DB interface.',
-  long_description=long_description,
-  long_description_content_type='text/markdown',
-  author = 'Tony Yum',
-  author_email = 'tony.yum@tayglobal.com',
-  url = 'https://github.com/tayglobal/kydb',
-  download_url = 'https://github.com/tayglobal/kydb/archive/v_01.tar.gz',
-  keywords = ['NoSQL', 'Database', 'DB'],
-  install_requires=[            # I get to this in a second
+    name='kydb',
+    packages=['kydb'],
+    version='0.2.2',
+    license='MIT',
+    description='kydb (Kinyu Database). NoSQL DB interface.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Tony Yum',
+    author_email='tony.yum@tayglobal.com',
+    url='https://github.com/tayglobal/kydb',
+    download_url='https://github.com/tayglobal/kydb/archive/v_01.tar.gz',
+    keywords=['NoSQL', 'Database', 'DB'],
+    install_requires=[            # I get to this in a second
         'boto3',
         'redis'
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',
-  ],
-  python_requires='>=3.8',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.8',
 )
