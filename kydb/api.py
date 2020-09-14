4133e3d0b90265147f1cda908fce890900606748
Tony Yum <tony.yum@tayglobal.com>
2020-09-14 13:29:34 +0000

    Support for new and upload_dbobj_config in UnionDB

--- kydb/api.py
+++ kydb/api.py
@@ -8,10 +8,38 @@ class UnionDB:
     def __init__(self, dbs: Tuple[BaseDB]):
         self.dbs = dbs
 
+    def upload_objdb_config(self, config):
+        """Upload ObjDB Conifg
+
+        :param config: The config dict
+
+        updates only the FrontDB
+        """
+        self.dbs[0].upload_objdb_config(config)
+
+    def new(self, *args, **kwargs):
+        """ Creates a new object
+
+        Calls new on the FrontDB
+        """
+        return self.dbs[0].new(*args, **kwargs)
+
     def exists(self, key: str):
+        """ key exists  in any of the union of of databases
+
+        :param key: the key to check existance of
+        """
         return any(db.exists(key) for db in self.dbs)
 
     def __getitem__(self, key: str):
+        """ get the object based on key
+
+
+        :param key: the key to get
+
+        attempts to resolve object from FrontDB to the back.
+        If not found, raise ``KeyError``
+        """
         for db in self.dbs:
             try:
                 return db[key]
@@ -21,6 +49,13 @@ class UnionDB:
         raise KeyError(key)
 
     def __setitem__(self, key, value):
+        """ set the object based on key
+
+
+        :param key: the key to set
+
+        saves the object in the FrontDB
+        """
         print(f'setting {key} to {value}')
         print(f'db = {self.dbs[0]}')
         self.dbs[0][key] = value
