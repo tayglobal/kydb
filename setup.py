227a6e9cbd4fadf4d3b92c2a9abcc58186dce313
Tony Yum <tony.yum@tayglobal.com>
2024-04-09 07:36:13 +0000

    feat(setup): Updated version to 0.7.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name='kydb',
     packages=['kydb', 'kydb.impl'],
-    version='0.7.4',
+    version='0.7.5',
     license='MIT',
     description='kydb (Kinyu Database). NoSQL DB interface.',
     long_description=long_description,
