a54dc536bfff6c5d29a19b10b2077259e8eaf382
Samurai Claw <artillerykai@gmail.com>
2026-09-02 15:46:52 +0000

    docs: add .readthedocs.yaml and update conf.py for Read the Docs v2

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -22,7 +22,8 @@ copyright = 'MIT License'
 author = 'Tony Yum'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+version = '1.0'
+release = '1.0.0'
 
 master_doc = 'index'
 
